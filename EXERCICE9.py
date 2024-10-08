d84edc550a89ccbc7ccf31e662e0a486ade34b75
NAJIB <najib.azmi2019@gmail.com>
2024-10-08 01:09:42 +0100

    Update EXE9

--- EXERCICE9.py
+++ EXERCICE9.py
@@ -1,5 +1,5 @@
 noms = ['Mohamed', 'Ahmed', 'Karima']
-tels = ['0612344567', '0734565658', '0612345610']
+tels = ['061234456', '0734565658', '0612345610']
 ids = [1, 2, 3] 
 dict_info = {}
 for i in range(len(ids)):
